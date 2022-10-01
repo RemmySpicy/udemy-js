f9e14cb660cd9427723862bb4af4fd1143c6b97d
O.Remilekun <remmylknolaotan@gmail.com>
2022-10-01 01:50:53 +0100

    Solve second challenge

--- function-practice/index.js
+++ function-practice/index.js
@@ -20,7 +20,6 @@ const poll = {
       this.displayResult(typeof answer);
     } else {
       console.log("Please enter a valid answer between 0 - 3");
-      this.displayResult(typeof this.answers);
     }
   },
 
@@ -39,8 +38,8 @@ document
 let bonus = {
   answers: [5, 2, 3],
 };
-console.log(poll.displayResult.call(bonus, "string"));
-console.log(poll.displayResult.call({ answers: [1, 5, 3, 9, 6, 1] }));
+poll.displayResult.call(bonus, "string");
+poll.displayResult.call({ answers: [1, 5, 3, 9, 6, 1] });
 
 /*
 1. Create a method called 'registerNewAnswer' on the 'poll' object. The method does 2 things:
@@ -62,3 +61,18 @@ BONUS TEST DATA 1: [5, 2, 3]
 BONUS TEST DATA 2: [1, 5, 3, 9, 6, 1]
 GOOD LUCK 😀
 */
+
+
+/*
+Take the IIFE below and at the end of the function, attach an event listener that changes the color of the selected h1 element ('header') to blue, each time the BODY element is clicked. Do NOT select the h1 element again!
+And now explain to YOURSELF (or someone around you) WHY this worked! Take all the time you need. Think about WHEN exactly the callback function is executed, and what that means for the variables involved in this example.
+GOOD LUCK 😀
+*/
+
+(function () {
+    const header = document.querySelector('h1');
+    header.style.color = 'red'
+
+    // console.log( header.parentElement.onclick() ) 
+    header.parentElement.addEventListener('click', () => header.style.color = 'blue'); 
+})();
