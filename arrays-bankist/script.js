cafa9bb3dc3c05060009bdbde99b20a79191243b
O.Remilekun <remmylknolaotan@gmail.com>
2022-10-04 23:51:23 +0100

    Complete calculatePrintBalance function

--- arrays-bankist/script.js
+++ arrays-bankist/script.js
@@ -109,7 +109,6 @@ function createUsernames(accs) {
 }
 
 createUsernames(accounts);
-console.log(accounts);
 
 const movementsDescription = movements.map(
   (mov, i) =>
@@ -118,7 +117,17 @@ const movementsDescription = movements.map(
     )}`
 );
 console.log(movementsDescription);
+
+function calculatePrintBalance(movements) {
+  const balance = movements.reduce((accu, mov) => accu + mov, 0);
+  labelBalance.textContent = `${balance} EUR`;
+}
+
+
+calculatePrintBalance(account1.movements);
+
 /////////////////////////////////////////////////
+
 
 /* 
 Julia and Kate are doing a study on dogs. So each of them asked 5 dog owners about their dog's age, and stored the data into an array (one array for each). For now, they are just interested in knowing whether a dog is an adult or a puppy. A dog is an adult if it is at least 3 years old, and it's a puppy if it's less than 3 years old.
