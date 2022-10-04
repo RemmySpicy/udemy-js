8797007d8bff285f0becec48d1549c3e47413072
O.Remilekun <remmylknolaotan@gmail.com>
2022-10-04 23:34:16 +0100

    Add createUsernames function

--- arrays-bankist/script.js
+++ arrays-bankist/script.js
@@ -74,13 +74,13 @@ const currencies = new Map([
 const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 const eurToUsd = 1.1;
-const movementsUSD = movements.map(mov => mov * eurToUsd);
+const movementsUSD = movements.map((mov) => mov * eurToUsd);
 console.log(movementsUSD);
 
 function displayMovements(movements) {
-    containerMovements.innerHTML = '';
-    
-    movements.forEach((mov, i) => {
+  containerMovements.innerHTML = "";
+
+  movements.forEach((mov, i) => {
     const type = mov > 0 ? "deposit" : "withdrawal";
 
     const html = `
@@ -90,15 +90,33 @@ function displayMovements(movements) {
             <div class="movements__value">${mov}</div>
         </div>
         `;
-        containerMovements.insertAdjacentHTML("afterbegin", html)
-    });
+    containerMovements.insertAdjacentHTML("afterbegin", html);
+  });
 }
 
-displayMovements(account1.movements)
+displayMovements(account1.movements);
 
+// Computing Usernames 
+function createUsernames(accs) {
+  // accs.forEach(acc => )
+  accs.forEach((acc) =>
+      acc.username = acc.owner
+        .toLowerCase()
+        .split(" ")
+        .map((name) => name[0])
+        .join("")
+  );
+}
 
+createUsernames(accounts);
+console.log(accounts);
 
-const movementsDescription = movements.map((mov, i) => `Movement ${i + 1}: You ${mov > 0 ? 'deposited' : 'withdrew'} ${Math.abs(mov)}`)
+const movementsDescription = movements.map(
+  (mov, i) =>
+    `Movement ${i + 1}: You ${mov > 0 ? "deposited" : "withdrew"} ${Math.abs(
+      mov
+    )}`
+);
 console.log(movementsDescription);
 /////////////////////////////////////////////////
 
