23e7b58d342ac0df6ec6bf3309d59a888564189e
O.Remilekun <remmylknolaotan@gmail.com>
2022-10-23 13:08:51 +0100

    Complete Challenge 4 task 1

--- arrays-bankist/script.js
+++ arrays-bankist/script.js
@@ -121,7 +121,7 @@ console.log(movementsDescription);
 // Display Account balance
 function calcDisplayBalance(acc) {
   acc.balance = acc.movements.reduce((acc, mov) => acc + mov, 0);
-  labelBalance.textContent = `${acc.balance} €`;
+  labelBalance.textContent = `${acc.balance} `;
 }
 
 // Maximum Value
@@ -272,9 +272,51 @@ btnSort.addEventListener('click', (e) => {
 
 
 
+/////// LESSONS ///////
 
 
+// Creating arrays programatially
 
+// General way
+// console.log([1, 2, 3, 4, 5, 6, 7]);
+const arr = [1, 2, 3, 4, 5, 6, 7];
+// or
+console.log(new Array(1, 2, 3, 4, 5, 6, 7));
+
+// programatically -- array constructors
+const x = new Array(7); // Creates empty array of length param
+console.log(x);
+console.log(x.map(() => 5)); // nothing happens, cos of new Array
+
+// Fill method -- mutates array like splice does
+// x.fill(1)    // or
+x.fill(1, 3, 5) 
+console.log(x);
+
+arr.fill(23, 2, 6);
+console.log(arr);
+
+// Best programatically
+// Array.from takes Map-like callback function
+const y = Array.from({length: 7}, () => 1)
+console.log(y);
+
+// _ means useless variable
+const z = Array.from({length: 7}, (_, i) => i + 1)
+console.log(z);
+
+// Get movements from UI
+labelBalance.addEventListener('click', () => {
+  const movementsUI = Array.from(
+    document.querySelectorAll('.movements__value'),
+    el => Number(el.textContent.replace('€', ''))
+  );
+
+  console.log(movementsUI);
+
+  // This also somehow create Array from 
+  const movementsUI2 = [...document.querySelectorAll('.movements__value')]
+})
 
 
 // Sorting arrays of number ---  sorting mutates array
@@ -284,17 +326,17 @@ btnSort.addEventListener('click', (e) => {
 
 // Ascending
 // movements.sort((a, b) => a > b ? 1 : -1);
-movements.sort((a, b) => a - b);
-console.log(movements);
+// movements.sort((a, b) => a - b);
+// console.log(movements);
 
 // Descending
 // movements.sort((a, b) => a > b ? -1 : 1);
-movements.sort((a, b) => b - a);
-console.log(movements);
+// movements.sort((a, b) => b - a);
+// console.log(movements);
 
 
 /////////////////////////////////////////////////
-
+///// CHALLENGES /////
 /* 
 Julia and Kate are doing a study on dogs. So each of them asked 5 dog owners about their dog's age, and stored the data into an array (one array for each). For now, they are just interested in knowing whether a dog is an adult or a puppy. A dog is an adult if it is at least 3 years old, and it's a puppy if it's less than 3 years old.
 Create a function 'checkDogs', which accepts 2 arrays of dog's ages ('dogsJulia' and 'dogsKate'), and does the following things:
@@ -345,3 +387,43 @@ const calcAverageHumanAge = ages => ages
 
 // console.log(calcAverageHumanAge([5, 2, 4, 1, 15, 8, 3]));
 // console.log(calcAverageHumanAge([16, 6, 10, 5, 6, 1, 4]));
+
+
+///////////////////////////////////////
+// Coding Challenge #4
+
+/* 
+Julia and Kate are still studying dogs, and this time they are studying if dogs are eating too much or too little.
+Eating too much means the dog's current food portion is larger than the recommended portion, and eating too little is the opposite.
+Eating an okay amount means the dog's current food portion is within a range 10% above and 10% below the recommended portion (see hint).
+1. Loop over the array containing dog objects, and for each dog, calculate the recommended food portion and add it to the object as a new property. Do NOT create a new array, simply loop over the array. Forumla: recommendedFood = weight ** 0.75 * 28. (The result is in grams of food, and the weight needs to be in kg)
+2. Find Sarah's dog and log to the console whether it's eating too much or too little. HINT: Some dogs have multiple owners, so you first need to find Sarah in the owners array, and so this one is a bit tricky (on purpose) 🤓
+3. Create an array containing all owners of dogs who eat too much ('ownersEatTooMuch') and an array with all owners of dogs who eat too little ('ownersEatTooLittle').
+4. Log a string to the console for each array created in 3., like this: "Matilda and Alice and Bob's dogs eat too much!" and "Sarah and John and Michael's dogs eat too little!"
+5. Log to the console whether there is any dog eating EXACTLY the amount of food that is recommended (just true or false)
+6. Log to the console whether there is any dog eating an OKAY amount of food (just true or false)
+7. Create an array containing the dogs that are eating an OKAY amount of food (try to reuse the condition used in 6.)
+8. Create a shallow copy of the dogs array and sort it by recommended food portion in an ascending order (keep in mind that the portions are inside the array's objects)
+HINT 1: Use many different tools to solve these challenges, you can use the summary lecture to choose between them 😉
+HINT 2: Being within a range 10% above and below the recommended portion means: current > (recommended * 0.90) && current < (recommended * 1.10). Basically, the current portion should be between 90% and 110% of the recommended portion.
+TEST DATA:
+const dogs = [
+  { weight: 22, curFood: 250, owners: ['Alice', 'Bob'] },
+  { weight: 8, curFood: 200, owners: ['Matilda'] },
+  { weight: 13, curFood: 275, owners: ['Sarah', 'John'] },
+  { weight: 32, curFood: 340, owners: ['Michael'] }
+];
+GOOD LUCK 😀
+*/
+
+const dogs = [
+  { weight: 22, curFood: 250, owners: ['Alice', 'Bob'] },
+  { weight: 8, curFood: 200, owners: ['Matilda'] },
+  { weight: 13, curFood: 275, owners: ['Sarah', 'John'] },
+  { weight: 32, curFood: 340, owners: ['Michael'] }
+];
+
+dogs.forEach(dog => {
+  dog.recommendedFood = dog.weight ** 0.75 * 28;
+  console.log(dog);
+});
