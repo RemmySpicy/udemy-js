2079aeb1c26b35b439a86882746f1cd26eeb8659
O.Remilekun <remmylknolaotan@gmail.com>
2022-11-02 23:00:14 +0100

    Add smooth scrolling to hero button

--- advanced-dom-bankist/script.js
+++ advanced-dom-bankist/script.js
@@ -7,6 +7,8 @@ const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 =  document.querySelector('#section--1');
 
 const openModal = function (e) {
   e.preventDefault();
@@ -30,8 +32,37 @@ document.addEventListener('keydown', function (e) {
   }
 });
 
+// Smooth scrolling
+
+btnScrollTo.addEventListener('click', e => {
+  const s1cords = section1.getBoundingClientRect();
+
+  console.log(s1cords);
+
+  // console.log(e.target.getBoundingClientRect());
+  // console.log(window.scrollX, window.scrollY);
+  // console.log('Height/width of viewport', document.documentElement.clientHeight, document.documentElement.clientWidth);
+
+  // Scrolling
+  // window.scrollTo(s1cords.left + window.scrollX, s1cords.top + window.scrollY);
+  
+  // window.scrollTo({
+  //   left: s1cords.left + window.scrollX,
+  //   top: s1cords.top + window.scrollY,
+  //   behavior: 'smooth',
+  // })
+
+  // Only in modern broswsers
+  section1.scrollIntoView({behavior: 'smooth'})
+})
+
+
+
+
+
 ///////////////////////
 ///////////////////////
+// Lessons
 
 // console.log(document.documentElement);
 // console.log(document.head);
@@ -40,5 +71,69 @@ document.addEventListener('keydown', function (e) {
 //  Creating and inserting elements
 // .insertAdjacentHTML
 
+// const header = document.querySelector('header');
+
 // const message = document.createElement('div');
 // message.classList.add('cookie-message');
+
+// message.innerHTML = "We use cookies for improved functionality and analytics <button class='btn btn--close-cookie'> Got it! </button>"
+
+// header.append(message)
+// document.querySelector('.btn--close-cookie')
+//   .addEventListener('click', () => {
+//   // message.parentElement.removeChild(message);
+//     message.remove();
+//   });
+
+// Styles
+// message.style.backgroundColor = '#37383d';
+// message.style.width = '104.75%';
+
+// console.log(getComputedStyle(message).color);
+// console.log(getComputedStyle(message).height);
+
+// message.style.height = Number.parseFloat(getComputedStyle(message).height) + 30 + 'px';
+
+// document.documentElement.style.setProperty('--color-primary', 'orangered')
+
+// // Attributes
+// const logo = document.querySelector('.nav__logo');
+// console.log(logo.src);
+// console.log(logo.getAttribute('src'));
+
+// // Data attribute
+// console.log(logo.dataset.versionNumber);
+
+// Classes
+// logo.classList.add();
+// logo.classList.remove();
+// logo.classList.toggle();
+// logo.classList.contains();
+
+/*
+// Smooth scrolling
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 =  document.querySelector('#section--1');
+
+btnScrollTo.addEventListener('click', e => {
+  const s1cords = section1.getBoundingClientRect();
+
+  console.log(s1cords);
+
+  // console.log(e.target.getBoundingClientRect());
+  // console.log(window.scrollX, window.scrollY);
+  // console.log('Height/width of viewport', document.documentElement.clientHeight, document.documentElement.clientWidth);
+
+  // Scrolling
+  // window.scrollTo(s1cords.left + window.scrollX, s1cords.top + window.scrollY);
+  
+  // window.scrollTo({
+  //   left: s1cords.left + window.scrollX,
+  //   top: s1cords.top + window.scrollY,
+  //   behavior: 'smooth',
+  // })
+
+  // Only in modern broswsers
+  section1.scrollIntoView({behavior: 'smooth'})
+})
+*/
