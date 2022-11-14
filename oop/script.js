25c295650d5f958638fb6690239b1248932e0729
O.Remilekun <remmylknolaotan@gmail.com>
2022-11-14 19:14:11 +0100

    Complete challenge 4

--- oop/script.js
+++ oop/script.js
@@ -65,6 +65,7 @@ class Carcl {
     break() {
         this.speed -= 5;
         console.log(`${this.make} is going at ${this.speed}km/h`);
+        return this;
     }
 
     get speedUS() {
@@ -126,3 +127,40 @@ tesla.accelerate();
 tesla.break();
 tesla.chargeBattery(90);
 tesla.accelerate();
+
+
+///////////////////////////////////////
+// Coding Challenge #4
+
+/* 
+1. Re-create challenge #3, but this time using ES6 classes: create an 'EVCl' child class of the 'CarCl' class
+2. Make the 'charge' property private;
+3. Implement the ability to chain the 'accelerate' and 'chargeBattery' methods of this class, and also update the 'brake' method in the 'CarCl' class. They experiment with chining!
+DATA CAR 1: 'Rivian' going at 120 km/h, with a charge of 23%
+GOOD LUCK 😀
+*/
+
+class EVcl extends Carcl {
+#charge;
+    constructor(make, speed, charge) {
+        super(make, speed);
+        this.#charge = charge;
+    }
+
+    chargeBattery(chargeTo) {
+        this.#charge = chargeTo;
+        return this;
+    }
+
+    accelerate() {
+        this.speed += 20;
+        this.#charge--;
+        console.log(`${this.make} is going at ${this.speed} km/h, with a charge of ${this.#charge}%`);
+        return this;
+    }
+}
+
+const rivian = new EVcl('Tesla', 120, 23);
+
+console.log(rivian.break().chargeBattery(90).accelerate());
+// console.log(rivian.#charge);
