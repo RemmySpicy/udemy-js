b2107ec5123b37947dea20a79db9ad1eaa89ba41
O.Remilekun <remmylknolaotan@gmail.com>
2022-11-15 03:20:29 +0100

    Refactoring code to classes to fit project architecture

--- mapty/script.js
+++ mapty/script.js
@@ -11,10 +11,25 @@ const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 
-let map, mapEvent;
 
-if (navigator.geolocation)
-    navigator.geolocation.getCurrentPosition((position) => {
+class App {
+    #map;
+    #mapEvent;
+
+    constructor(){
+        this._getPosition();
+        form.addEventListener('submit', this._newWorkout.bind(this));
+        inputType.addEventListener('change', this._toggleElevationField);
+    }
+
+    _getPosition() {
+        if (navigator.geolocation)
+            navigator.geolocation.getCurrentPosition(this._loadMap.bind(this)), () => {
+                console.log('Could not get location');
+            };
+    }
+
+    _loadMap(position) {
         const {latitude} = position.coords;
         const {longitude} = position.coords;
 
@@ -22,62 +37,49 @@ if (navigator.geolocation)
 
         const coords = [latitude, longitude]
 
-        map = L.map('map').setView(coords, 13);
+        this.#map = L.map('map').setView(coords, 13);
 
         L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
             attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        }).addTo(map);
-
-        // L.marker(coords).addTo(map)
-        //     .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
-        //     .openPopup();
+        }).addTo(this.#map);
 
         // Handling clicks on map
-        map.on('click', (mapE) => {
-            mapEvent = mapE;
-            form.classList.remove('hidden');
-            inputDistance.focus()
-            // console.log(mapEvent);
-            // const {lat, lng} = mapEvent.latlng;
+        this.#map.on('click', this._showForm.bind(this));
+    }
 
-            // L.marker([lat, lng]).addTo(map)
-            //     .bindPopup(L.popup({
-            //         maxWidth: 250,
-            //         minWidth: 300,
-            //         autoClose: false,
-            //         closeOnClick: false,
-            //         className: 'running-popup'
-            //     }))
-            //     .setPopupContent('Workout')
-            //     .openPopup();
-        });
-    }, () => {
-        console.log('Could not get location');
-    });
+    _showForm(mapE) {
+        this.#mapEvent = mapE;
+        form.classList.remove('hidden');
+        inputDistance.focus()
+    }
 
-    
-    form.addEventListener('submit', (e) => {
+    _toggleElevationField() {
+        inputElevation.closest('.form__row').classList.toggle('form__row--hidden')
+        inputCadence.closest('.form__row').classList.toggle('form__row--hidden')
+    }
+
+    _newWorkout(e) {
         e.preventDefault();
-
+    
         // Clear input fields
         inputDistance.value = inputCadence.value = inputDuration.value = inputElevation.value = '';
 
         // Display marker
-    const {lat, lng} = mapEvent.latlng;
+        const {lat, lng} = this.#mapEvent.latlng;
+    
+        L.marker([lat, lng]).addTo(this.#map)
+            .bindPopup(L.popup({
+                maxWidth: 250,
+                minWidth: 300,
+                autoClose: false,
+                closeOnClick: false,
+                className: 'running-popup'
+            }))
+            .setPopupContent('Workout')
+            .openPopup();
+    }
+}
 
-    L.marker([lat, lng]).addTo(map)
-        .bindPopup(L.popup({
-            maxWidth: 250,
-            minWidth: 300,
-            autoClose: false,
-            closeOnClick: false,
-            className: 'running-popup'
-        }))
-        .setPopupContent('Workout')
-        .openPopup();
-})
-
-inputType.addEventListener('change', () => {
-    inputElevation.closest('.form__row').classList.toggle('form__row--hidden')
-    inputCadence.closest('.form__row').classList.toggle('form__row--hidden')
-})
+const app = new App();
+    
+    
