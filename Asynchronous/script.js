8555be26480f6da035d624314cea50cdee027aba
O.Remilekun <remmylknolaotan@gmail.com>
2022-11-26 23:20:00 +0100

    refactor code into function

--- Asynchronous/script.js
+++ Asynchronous/script.js
@@ -5,28 +5,34 @@ const countriesContainer = document.querySelector('.countries');
 
 ///////////////////////////////////////
 
-const request = new XMLHttpRequest();
+const getCountryData = (country) => {
 
-request.open('GET', 'https://restcountries.com/v3.1/name/india');
-request.send();
+    const request = new XMLHttpRequest();
 
-request.addEventListener('load', function() {
-    const [data] = JSON.parse(this.responseText)
-    console.log(data);
+    request.open('GET', `https://restcountries.com/v3.1/name/${country}`);
+    request.send();
 
-    const html = `
-    <article class="country">
-        <img class="country__img" src="${data.flags.png}" />
-        <div class="country__data">
-            <h3 class="country__name">${data.name.common}</h3>
-            <h4 class="country__region">${data.region}</h4>
-            <p class="country__row"><span>👫</span>${(+data.population / 1000000).toFixed(1)} M</p>
-            <p class="country__row"><span>🗣️</span>${Object.entries(data.languages)[0][1]}</p>
-            <p class="country__row"><span>💰</span>${Object.entries(data.currencies)[0][1].name}</p>
-        </div>
-    </article>
-    `;
+    request.addEventListener('load', function() {
+        const [data] = JSON.parse(this.responseText)
+        console.log(data);
 
-    countriesContainer.insertAdjacentHTML('beforeend', html);
-    countriesContainer.style.opacity = 1;
-})
+        const html = `
+        <article class="country">
+            <img class="country__img" src="${data.flags.png}" />
+            <div class="country__data">
+                <h3 class="country__name">${data.name.common}</h3>
+                <h4 class="country__region">${data.region}</h4>
+                <p class="country__row"><span>👫</span>${(+data.population / 1000000).toFixed(1)} Million People</p>
+                <p class="country__row"><span>🗣️</span>${Object.entries(data.languages)[0][1]}</p>
+                <p class="country__row"><span>💰</span>${Object.entries(data.currencies)[0][1].name}</p>
+            </div>
+        </article>
+        `;
+
+        countriesContainer.insertAdjacentHTML('beforeend', html);
+        countriesContainer.style.opacity = 1;
+    })
+};
+
+getCountryData('nigeria');
+getCountryData('ghana');
