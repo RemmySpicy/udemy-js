2c7399e67cf17de8fad22c7c35eb2d1959734e05
O.Remilekun <remmylknolaotan@gmail.com>
2022-11-28 23:27:38 +0100

    Add renderError func, renderCountry func, .then().catch().finally()

--- Asynchronous/script.js
+++ Asynchronous/script.js
@@ -3,6 +3,28 @@
 const btn = document.querySelector('.btn-country');
 const countriesContainer = document.querySelector('.countries');
 
+const renderError = (msg) => {
+    countriesContainer.insertAdjacentText("beforeend", msg)
+}
+
+const renderCountry = (data, className = '') => {
+    const html = `
+        <article class="country ${className}">
+            <img class="country__img" src="${data.flags.png}" />
+            <div class="country__data">
+                <h3 class="country__name">${data.name.common}</h3>
+                <h4 class="country__region">${data.region}</h4>
+                <p class="country__row"><span>👫</span>${(+data.population / 1000000).toFixed(1)} Million People</p>
+                <p class="country__row"><span>🗣️</span>${Object.entries(data.languages)[0][1]}</p>
+                <p class="country__row"><span>💰</span>${Object.entries(data.currencies)[0][1].name}</p>
+            </div>
+        </article>
+        `;
+
+    countriesContainer.insertAdjacentHTML('beforeend', html);
+}
+
+
 ///////////////////////////////////////
 
 // const getCountryData = (country) => {
@@ -33,24 +55,6 @@ const countriesContainer = document.querySelector('.countries');
 //         countriesContainer.style.opacity = 1;
 //     })
 // };
-
-const renderCountry = (data, className = '') => {
-    const html = `
-        <article class="country ${className}">
-            <img class="country__img" src="${data.flags.png}" />
-            <div class="country__data">
-                <h3 class="country__name">${data.name.common}</h3>
-                <h4 class="country__region">${data.region}</h4>
-                <p class="country__row"><span>👫</span>${(+data.population / 1000000).toFixed(1)} Million People</p>
-                <p class="country__row"><span>🗣️</span>${Object.entries(data.languages)[0][1]}</p>
-                <p class="country__row"><span>💰</span>${Object.entries(data.currencies)[0][1].name}</p>
-            </div>
-        </article>
-        `;
-
-    countriesContainer.insertAdjacentHTML('beforeend', html);
-    countriesContainer.style.opacity = 1;
-}
 
 // const getCountryAndNeighbour = (country) => {
 
@@ -130,7 +134,16 @@ const getCountryData = (country) => {
             return fetch(`https://restcountries.com/v3.1/alpha/${neighbour}`)
         })
         .then(response => response.json())
-        .then(data => renderCountry(data[0], 'neighbour'));
+        .then(data => renderCountry(data[0], 'neighbour'))
+        .catch(err => {
+            console.alert(`${err} 💥`);
+            renderError(`Something went wrong 💥 ${err.message} Try again`);
+        })
+        .finally(() => {
+            countriesContainer.style.opacity = 1;
+        });
 }
 
-getCountryData('nigeria')
+btn.addEventListener('click', function() {
+    getCountryData('nigeria')
+})
