1478530926b17279e5ab99fc9c106653ce0b8563
O.Remilekun <remmylknolaotan@gmail.com>
2023-01-26 23:34:15 +0100

    Add import and exporting lessons

---
+++ modern-js-modules-tooling/script.js
@@ -0,0 +1,21 @@
+// Importing Module
+// import { addToCart, totalPrice as price, qt } from "./shoppingCart.js"; 
+// addToCart('bread', 5);
+// console.log(price, qt);
+
+console.log('Importing module');
+
+// import * as ShoppingCart from "./shoppingCart.js";
+
+// ShoppingCart.addToCart('bread', 5);
+// console.log(ShoppingCart.totalPrice);
+
+// import add, { addToCart, totalPrice as price, qt } from './shoppingCart.js';
+// console.log(price);
+
+import add, { cart } from './shoppingCart.js';
+add('pizza', 2)
+add('bread', 5)
+add('apple', 4)
+
+console.log(cart);
