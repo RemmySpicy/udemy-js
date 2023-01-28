33ad8190129296c28f951042aa52f5ab358d7ef3
O.Remilekun <remmylknolaotan@gmail.com>
2023-01-28 20:30:43 +0100

    Add cleaned clean.js file

--- modern-js-modules-tooling/script.js
+++ modern-js-modules-tooling/script.js
@@ -5,6 +5,7 @@
 
 console.log('Importing module');
 
+import shoppingCart from './shoppingCart.js';
 // import * as ShoppingCart from "./shoppingCart.js";
 
 // ShoppingCart.addToCart('bread', 5);
@@ -14,11 +15,11 @@ console.log('Importing module');
 // console.log(price);
 
 import add, { cart } from './shoppingCart.js';
-add('pizza', 2)
-add('bread', 5)
-add('apple', 4)
+// add('pizza', 2)
+// add('bread', 5)
+// add('apple', 4)
 
-console.log(cart);
+// console.log(cart);
 
 // console.log('Fetching starting');
 // const res = await fetch('https://jsonplaceholder.typicode.com/posts');
@@ -37,11 +38,48 @@ const getLastPost = async function() {
     }
 }
 
-const lastPost = getLastPost()
+// const lastPost = getLastPost()
 
 // Non blocking but not very clean
 // lastPost.then(res => console.log(res))
 // console.log(lastPost);
 
-const lastPost2 = await getLastPost()
-console.log(lastPost2);
+// const lastPost2 = await getLastPost()
+// console.log(lastPost2);
+
+// const ShoppingCart2 = (function() {
+//     const cart = [];
+//     const shippingCost = 10;
+//     const totalPrice = 237;
+//     const totalQuantity = 23;
+
+//     const addToCart = function(product, quantity) {
+//         cart.push({product, quantity});
+//         console.log(`${quantity} ${product} was added to the cart. (shipping cost is ${shippingCost})`);
+//     }
+    
+//     const orderStock = function() {
+//         console.log(`${quantity} ${product} ordered from supplier`);
+//     }
+
+//     return {
+//         addToCart,
+//         cart,
+//         totalPrice,
+//         totalQuantity,
+//     }
+// })()
+
+
+// ShoppingCart2.addToCart('apple', 4)
+// ShoppingCart2.addToCart('pizza', 2)
+// console.log(ShoppingCart2.shippingCost);
+
+// Export in commonJS
+// export.addToCart = function(product, quantity) {
+//     cart.push({product, quantity});
+//     console.log(`${quantity} ${product} was added to the cart. (shipping cost is ${shippingCost})`);
+// }
+
+// Import in commonJS
+// const { addToCart } = require('./shoppingCart.js');
